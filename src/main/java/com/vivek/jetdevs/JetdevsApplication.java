96a78d5316fba8a5a10bde4636f6c183e486c37a
Vivek <vivek.patel@xceltec.com>
2022-03-03 13:31:52 +0530

    initial commit

---
+++ src/main/java/com/vivek/jetdevs/JetdevsApplication.java
@@ -0,0 +1,13 @@
+package com.vivek.jetdevs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetdevsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JetdevsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivek/jetdevs/JetdevsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.jetdevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JetdevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
